b2d1998477a34048e98419c9db66617e4a046fc6
s4tyendra <satya@satyendra.in>
2025-11-29 11:04:39 +0530

    Project init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <p class="bg-red-500">Hello World</p>
+</template>
