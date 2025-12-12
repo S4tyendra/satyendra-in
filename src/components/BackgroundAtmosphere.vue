683d25bf8ed58da064c8581ec7623bb2cc0030f9
s4tyendra <satya@satyendra.in>
2025-12-12 16:21:47 +0530

    refactor: adjust z-index values for background and noise overlay components.

--- src/components/BackgroundAtmosphere.vue
+++ src/components/BackgroundAtmosphere.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <!-- Main Background -->
-    <div class="fixed top-0 left-0 w-full h-full z-0 bg-bg-main/20 "></div>
+    <div class="fixed top-0 left-0 w-full h-full z-[-1] bg-bg-main/20 "></div>
     <!-- Green blur -->
-    <div class="fixed top-[-20%] right-[-10%] w-[500px] h-[500px] bg-green-500/5 rounded-full blur-[100px] pointer-events-none z-0 animate-blob"></div>
+    <div
+      class="fixed top-[-20%] right-[-10%] w-[500px] h-[500px] bg-green-500/5 rounded-full blur-[100px] pointer-events-none z-0 animate-blob">
+    </div>
     <!-- Blue blur -->
-    <div class="fixed bottom-[-10%] left-[-10%] w-[500px] h-[500px] bg-blue-500/5 rounded-full blur-[100px] pointer-events-none z-0 animate-blob animation-delay-2000"></div>
+    <div
+      class="fixed bottom-[-10%] left-[-10%] w-[500px] h-[500px] bg-blue-500/5 rounded-full blur-[100px] pointer-events-none z-0 animate-blob animation-delay-2000">
+    </div>
     <!-- Noise overlay -->
     <div class="fixed top-0 left-0 w-full h-full -z-10 dot-bg "></div>
   </div>
